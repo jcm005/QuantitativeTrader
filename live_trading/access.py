ff6497c218818532259414c2d4ec36ac3fbb9bc4
jcm005 <joseph.mattern@stonybrook.edu>
2020-06-06 21:08:29 -0400

    hidden txt files

---
+++ live_trading/access.py
@@ -0,0 +1,121 @@
+import requests, json
+from streamkeys import *
+import alpaca_trade_api as tradeapi
+from datetime import datetime
+#for non paper we can use tradeapi.REST(insert keys here)
+
+BASE_URL = "https://paper-api.alpaca.markets"
+ACCOUNT_URL = f'{BASE_URL}/v2/account'
+ORDERS_URL = f'{BASE_URL}/v2/orders'
+POSITIONS_URL = f'{BASE_URL}/v2/positions'
+POSITION_FOR_URL = f'{BASE_URL}/v2/positions/'
+HEADERS = {'APCA-API-KEY-ID': PAPER_KEY, 'APCA-API-SECRET-KEY':SECRET__KEY}
+
+#requests.get(ACCOUNT_URL,headers=HEADERS)
+#api = tradeapi.REST(API_KEY, SECRET_KEY, api_version='v2')
+
+
+def clean_and_load(message):
+    # DO NOT MODIFY THIS CODE BELOW THIS CLEANS THE INPUT !!!!
+    message = message.strip(message[0])
+    message = message.strip(message[-1])
+    return message
+    # DO NOT MODIFY THIS CODE ABOVE THIS CLEANS THE INPUT !!!!
+    # LOADS AND CONVERTS DATETIME ALLOCATION OF SOME ALIAS
+def byte_decoder(need_decoded):
+    '''DECODES THE OUTPUT OF A REQUEST.GET()
+     FUNCTION RETURNING A DICTIONARY'''
+    decoded = need_decoded.content.decode('utf-8')
+    dumper = json.loads(decoded)
+    return dumper
+def get_account():
+    '''For now returns all account infor this function can
+    be used to clean the outputted information
+    PRINTING ACCOUNT WILL GIVE US RESPONSE CODE
+    '''
+    account = requests.get(ACCOUNT_URL,headers=HEADERS)
+    return byte_decoder(account)
+def get_orders():
+    '''Catered to getting any open positions '''
+    preface = requests.get(ORDERS_URL,headers=HEADERS)
+    return byte_decoder(preface)
+def get_position():
+    custom_position = requests.get(POSITIONS_URL,headers=HEADERS)
+    return byte_decoder(custom_position)
+def get_position_for(symbol):
+    '''IF no position function will return a dictionary with
+    a message saying there is no position therefore
+    we will make the function return a false
+    IF there is a position the function  will
+     return the positions'''
+    position_for = requests.get(f'{POSITION_FOR_URL}{symbol}',headers=HEADERS)
+    decoded =  byte_decoder(position_for)
+    try:
+        if decoded['message'] == 'position does not exist':
+            return False
+
+    except:
+        return decoded
+def place_order(order):
+    '''
+    SENDS ORDER TO ALPACA FOR PROCESSING
+    :param order: DICTIONARY PERFERABLLY JSON FORMAT
+    :return: DICTIONARY THAT WAS OUTPUTTED FROM BYTE_DECODEDR AND A BOOLEAN BASED ON SUCCESSION OR NOT
+    '''
+    preface = requests.post(ORDERS_URL,json=order,headers=HEADERS)
+    return byte_decoder(preface),True
+def order_details(raw_order,output='detailed'):
+    '''
+    :param raw_order: PASS IN A DICTIONARY GIVEN BY PLACE ORDER
+    :param output: PASS 'simple' FOR BASIC ORDER INFO OR 'detailed' for detailed out
+    :return: WILL RETURN ORDER DETAILS AS A DICT
+    '''
+
+
+    if output == 'simple':
+        simple = {
+            'symbol':raw_order['symbol'],
+            'status':raw_order['status'],
+            'order_type':raw_order['order_type'],
+            'side':raw_order['side'],
+            #'submitted_at': time,
+            'order_id':raw_order['id'],
+        }
+        return simple
+    elif output == 'detailed':
+        return raw_order
+
+date_time = '2020-05-21T13:59:03.266008Z'
+date = date_time[:10]
+
+
+
+# CODE FOR STREAM 2
+symbol = 'TSLA'
+qty = 1
+side = 'sell'
+order_type = 'limit'
+time_in_force= 'day'
+limit_price = 900
+stop_price = 800
+stop_limit_price = 799
+
+order = {
+    'symbol': symbol,
+    'qty': qty,
+    'side': side,
+    'type': order_type,
+    'time_in_force': 'gtc',
+
+    # append this into the dictionary if order type is limit or stop limit
+    # 'stop_price':int,
+    # https://alpaca.markets/docs/api-documentation/api-v2/orders/
+}
+
+
+order['limit_price'] = limit_price
+
+
+
+#position = get_position_for('TSLA')
+#print(position)
