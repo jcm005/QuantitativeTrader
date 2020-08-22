32180d51a6b2745075580a930f3ed56a2862e6c0
jcm005 <joseph.mattern@stonybrook.edu>
2020-08-21 20:45:53 -0400

    workon garbage.py for replacement of stream2.py

--- live_trading/garbage.py
+++ live_trading/garbage.py
@@ -1,6 +1,228 @@
-import alpaca_trade_api as tradeapi
+import websocket, json, requests, sys
 from streamkeys import *
-api = tradeapi.REST('AKF7BISHSY3SBP0M2IS8','URqDMkUgsj4aC9YMw3VwGizmiMfdhbZFPcKb5Gx4', api_version='v2')
+import ssl
+from datetime import datetime
+import dateutil.parser
+import access as a
+import time
+import pytz
+from pytz import timezone
+from tesla import *
 
-response = api.polygon.historic_agg_v2('TSLA', 1, 'day', '2020-06-01', '2020-06-07')
-print(response)
+est = pytz.timezone('US/Eastern')
+minutes_processed = {}
+minute_candlestick = []
+current_tick = None
+previous_tick = None
+in_position = False
+
+candles = open('candle.txt', 'a')
+connection_log = open('log_on.txt', 'a')
+log = open('action.txt', 'a')
+
+candles.truncate(0)
+log.truncate(50)
+
+def _reopen(file):
+    file_to_repoen = open(file, 'a')
+    return file_to_repoen
+
+def time_converter(some_time):
+
+    newtime = datetime.fromtimestamp(some_time / 1000)
+
+    newtimes = newtime.strftime('%Y-%m-%d, %a, %H:%M')
+    return newtimes
+
+def intiate_order(
+        symbol,
+        qty=1,
+        order_type='market',
+        side=None,
+        time_in_force='day',
+        limit_price=None,
+        stop_price=None,
+        order_class=None,
+        stop_limit_price=None):
+    '''INTIATION OF AN ORDER TO BE PASSED IN ACCESS.PY PLACE_ORDER COMMAND'''
+    global log
+    log.write('Initiating Order\n')
+    order = {
+        'symbol': symbol,
+        'qty': qty,
+        'side': side,
+        'type': order_type,
+        'time_in_force': time_in_force,
+        # append this into the dictionary if order type is limit or stop limit
+        # 'stop_price':int,
+        # https://alpaca.markets/docs/api-documentation/api-v2/orders/
+    }
+
+    if order_class == 'bracket':
+        order['order_class'] = 'bracket'
+        order['take_profit'] = {
+            'limit_price': limit_price
+        }
+        order['stop_loss'] = {
+            'stop_price': stop_price,
+            'limit_price': stop_limit_price
+        }
+    # OCO ORDER CURRENTLY NOT WORKING HOWEVER NOT MY MAIN CONCERN ATM
+
+    elif order_class == 'oco':
+        order['type'] = order_type
+        order['order_class'] = 'oco'
+        order['take_profit'] = {
+            'limit_price': limit_price
+        }
+        order['stop_loss'] = {
+            'stop_price': stop_price,
+            'limit_price': stop_limit_price
+        }
+        return order
+    else:
+        pass
+
+    if order_type == 'market':
+        return order
+    if order_type == 'limit':
+        order['limit_price'] = limit_price
+        return order
+    if order_type == 'stop':
+        order['limit_price'] = limit_price
+        order['stop_price'] = stop_price
+        return order
+
+
+def order_sequence(order,
+                   current_price,
+                   order_details='detailed'):
+    '''
+     FUNCTION TAKES IN A INITIATED ORDER CREATES THE SELL ORDER PAIRING WITH LIMIT
+     SET AT SELL_OFF_POINT AND RETURNS DETAILED ORDER WHETHER SIMPLE OR ORIGINAL
+
+    :param order: INITIATED_ORDER SEE STREAM2.PY
+    :param sell_off_point: YOUR TAKE PROFIT PARAMETER
+    :param order_details: SIMPLE OR DETAILED ORDER INFOMATION DEFUALT == DETIALED
+    :return: BUY AND SELL AS ORDER DETAILS
+    '''
+    order_buy = order
+    global log
+    _reopen(log)
+    log.write('Sending Order\n')
+    try:
+        ordered_B, status = a.place_order(order_buy)
+        buy = a.order_details(ordered_B, order_details)
+    except:
+        buy = 'Order_Buy_Failed'
+    # --------------------------
+    #       Sell Order
+    # --------------------------
+    order_sell = order
+    order_sell['side'] = 'sell'
+    order_sell['type'] = 'limit'
+    # --------------------------------------------------
+    #  INITIATING PROFIT MARGINS
+    # --------------------------------------------------
+    if current_price >= 800:
+        order_sell['limit_price'] = current_price + 100
+    if 500 < current_price < 800:
+        order_sell['limit_price'] = current_price + 50
+    if 200 < current_price < 500:
+        order_sell['limit_price'] = current_price + 30
+    try:
+        log.write(f'Order Sell = :{order_sell}')
+        ordered_S, status = a.place_order(order_sell)
+        sell = a.order_details(ordered_S, order_details)
+        log.write(f'sell  = :{sell}')
+    except:
+        sell = 'Order_Sell_Failed'
+
+    return buy, sell
+
+
+def check_time():
+    connection_log = _reopen('log_on.txt')
+    print('Checking Time')
+    connection_log.write('Checking Time\n')
+
+    tiz = timezone('US/Eastern')
+    right_now = pytz.utc.localize(datetime.utcnow()).astimezone(tiz)
+    right_now = datetime.strftime(right_now, '%H:%M:%S')
+    print((right_now[:2]))
+    connection_log.write(f'{right_now[:2]}\n')
+    if 6  < int(right_now[:2]) < 23:
+        print('Good... reconnecting')
+        connection_log.write(f'Good..Reconnecting, the time is :{right_now}\n')
+        try:
+            socket = "wss://alpaca.socket.polygon.io/stocks"
+            ws = websocket.WebSocketApp(socket,
+                                        on_open=onn_open,
+                                        on_message=tesla,
+                                        on_error=on_error,
+                                        on_close=on_close
+                                        )
+            ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+        except:
+            print('Connection Failed')
+            connection_log.write('Connection Failed')
+
+            try:
+                socket = "wss://alpaca.socket.polygon.io/stocks"
+                ws = websocket.WebSocketApp(socket,
+                                            on_open=onn_open,
+                                            on_message=tesla,
+                                            on_error=on_error,
+                                            on_close=on_close
+                                            )
+                ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+            except:
+                print('Connection Failed')
+    else:
+        print('The Day Has Ended')
+        connection_log.write('The day has ended\n')
+# ----------------------------WEB-SOCKET FUNCTIONS BELOW ------------------
+def onn_open(ws):
+    connection_log = _reopen('log_on.txt')
+    print("\nConnecting...")
+    auth_data = {
+        "action": "auth",
+        "params": PAPER_KEY
+    }
+    ws.send(json.dumps(auth_data))
+    channel_data = {
+        "action": "subscribe",
+        "params": "AM.TSLA"
+    }
+    ws.send(json.dumps(channel_data))
+    print("\nConnected...")
+    connection_log.write(f'Logged In @ {datetime.now()}\n')
+    connection_log.close()
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws):
+    global connection_log
+    tiz = timezone('US/Eastern')
+    right_now = pytz.utc.localize(datetime.utcnow()).astimezone(tiz)
+    right_now = datetime.strftime(right_now, '%H:%M:%S')
+    log = _reopen('action.txt')
+    connection_log = _reopen('log_on.txt')
+    log.write('Lost Connection See Log_on.txt\n')
+    connection_log.write(f'Connection Closed {right_now} \nWorking on Re-establishing Connection...@{datetime.now()}\n')
+    check_time()
+    connection_log.close()
+    log.close()
+# -----------------------------
+
+
+if __name__ == '__main__':
+    socket = "wss://alpaca.socket.polygon.io/stocks"
+    ws = websocket.WebSocketApp(socket,
+                                on_open=onn_open,
+                                on_message=tesla,
+                                on_error=on_error,
+                                on_close=on_close
+                                )
+    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
--- live_trading/ordering_test.py
+++ live_trading/ordering_test.py
@@ -1,19 +1,19 @@
 from order import Order
 
 if __name__ == '__main__':
-    current_price = 2070
+    current_price = 2030
     tsla = Order('TSLA', current_price)
 
 
     buy = tsla.buy(order_class='bracket',
                    order_type='market',
-                   qty='1', tif='gtc',
+                   qty=1, tif='gtc',
                 # USE THIS ONLY FOR LIMIT TYPE ORDER
                    limit_price=0,
                 # UNCOMMENT, FOR THE USE OF BRACKETT ORDERS
                    profit=100,
-                   stop_limit_price=current_price - 2,
-                   stop_price=current_price - .5
+                   stop_limit_price=current_price - 51,
+                   stop_price=current_price - 40
                    )
 print(buy)
 
